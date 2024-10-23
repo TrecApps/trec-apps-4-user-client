fe7d1f73e1b810f4decdb6654c5e439d041c363e
John Jacko <jljacko@outlook.com>
2024-10-22 20:19:46 -0500

    Added Address Service

---
+++ src/app/services/address.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Address, AuthService } from 'tc-ngx-general';
+import { environment } from '../Environment/environment';
+import { ResponseObj } from 'tc-ngx-general/lib/models/ResponseObj';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor(private authService: AuthService, private client: HttpClient) { }
+
+  searchAddress(query: string): Observable<Address[]> {
+    let q = query.trim();
+    if(q.length < 10) return of([]);
+
+    
+
+
+    return this.client.get<Address[]>(`${environment.user_subscription_url}/Address/search`,{
+      params: new HttpParams().append("query", q),
+      headers: this.authService.getHttpHeaders(false, false)
+    });
+  }
+
+  postAddress(address: Address): Observable<ResponseObj> {
+    return this.client.post<ResponseObj>(`${environment.user_subscription_url}/Address`, address, {
+      headers: this.authService.getHttpHeaders(false, false)
+    });
+  }
+
+  setStatus(index: number, isMailing: boolean): Observable<ResponseObj> {
+    return this.client.put<ResponseObj>(`${environment.user_subscription_url}/Address/${isMailing ? 'Mailing': 'Billing'}`, null, {
+      headers: this.authService.getHttpHeaders(false, false),
+      params: new HttpParams().append("index", index)
+  });
+  }
+
+  removeAddress(index: number): Observable<ResponseObj> {
+    return this.client.delete<ResponseObj>(`${environment.user_subscription_url}/Address`, {
+      headers: this.authService.getHttpHeaders(false, false),
+      params: new HttpParams().append("index", index)
+  });
+  }
+
+  unhookAddress(isMailing: boolean): Observable<ResponseObj> {
+    return this.client.delete<ResponseObj>(`${environment.user_subscription_url}/Address`, {
+      headers: this.authService.getHttpHeaders(false, false),
+      params: new HttpParams().append("index", isMailing ? 'Mailing': 'Billing')
+  });
+  }
+}
