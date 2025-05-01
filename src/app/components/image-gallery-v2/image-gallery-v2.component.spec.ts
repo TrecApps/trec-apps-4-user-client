c8363f7662f5f7b196014f14b5f89ea527c15669
John Jacko <jljacko@outlook.com>
2025-05-01 13:49:19 -0600

    Began working on new Image Gallery Component

---
+++ src/app/components/image-gallery-v2/image-gallery-v2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageGalleryV2Component } from './image-gallery-v2.component';
+
+describe('ImageGalleryV2Component', () => {
+  let component: ImageGalleryV2Component;
+  let fixture: ComponentFixture<ImageGalleryV2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ImageGalleryV2Component]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ImageGalleryV2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/image-gallery-v2/image-gallery-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-gallery-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-gallery-v2.component.html',
+  styleUrl: './image-gallery-v2.component.css'
+})
+export class ImageGalleryV2Component {
+
+}
